603960ff17b1281650a8a5403350aca564382120
salilshukla07 <viratshukla963@gmail.com>
2023-07-18 10:27:13 +0530

    testing purpose

---
+++ application/logs/log-2022-10-27.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-10-27 12:48:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'hms_db' C:\xampp\htdocs\hotal\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2022-10-27 12:48:33 --> Unable to connect to the database
+ERROR - 2022-10-27 13:23:13 --> Severity: Notice --> Undefined variable: customer_id C:\xampp\htdocs\hotal\application\views\reservation\list.php 15
